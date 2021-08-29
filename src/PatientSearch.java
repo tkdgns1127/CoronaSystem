44e9cd63e6026837fa04589d531041b3d27196c8
hyunnndo <andy.hyundo@gmail.com>
2021-08-29 15:39:17 +0900

    예외처리수정 (기능 한개 미완성)

--- src/PatientSearch.java
+++ src/PatientSearch.java
@@ -11,67 +11,67 @@ public class PatientSearch {
 	public void personSearch(String name) {
 		System.out.println("personSearch 메소드 실행");
 		// 확진자 리스트에서 조회
-//		for (Person person : Test.infectionList) {
-//			if (name.equals(person.getName())) {
-//				System.out.println("이름 : " + person.getName());
-//				System.out.println("확진여부 : 확진");
-//				System.out.println("나이 : " + person.getAge());
-//				System.out.println("번호  : " + person.getPhoneNum());
-//			}
-//		}
-//
-//		// 치료자 리스트에서 조회
-//		for (Person person : Cure.treatmentCompletedList) {
-//			if (name.equals(person.getName())) {
-//				System.out.println("이름 : " + person.getName());
-//				System.out.println("확진여부 : 완치");
-//				System.out.println("나이 : " + person.getAge());
-//				System.out.println("번호  : " + person.getPhoneNum());
-//			}
-//		}
-//
-//		// 사망자 리스트에서 조회
-//		for (Person person : Cure.deathList) {
-//			if (name.equals(person.getName())) {
-//				System.out.println("이름 : " + person.getName() + " (사망)");
-//				System.out.println("나이 : " + person.getAge());
-//				System.out.println("번호  : " + person.getPhoneNum());
-//			}
-//		}
-
-		// 조회 결과가 없다면 이거 어떻게 해야되냐
-		for (Person person1 : Test.infectionList) {
-			if (!(name.equals(person1.getName()))) {
-				System.out.println("infectionList에 해당 이름 없음");
-				for (Person person2 : Cure.treatmentCompletedList) {
-					if (!(name.equals(person2.getName()))) {
-
-						for (Person person3 : Cure.deathList) {
-							if (!(name.equals(person3.getName()))) {
-								System.out.println("검색 결과가 없습니다.");
-							
-							} else {
-								System.out.println("이름 : " + person3.getName() + " (사망)");
-								System.out.println("나이 : " + person3.getAge());
-								System.out.println("번호  : " + person3.getPhoneNum());
-							}
-						}
-
-					} else {
-						System.out.println("이름 : " + person2.getName());
-						System.out.println("확진여부 : 완치");
-						System.out.println("나이 : " + person2.getAge());
-						System.out.println("번호  : " + person2.getPhoneNum());
-					}
-				}
-				
-			} else {
-				System.out.println("이름 : " + person1.getName());
+		for (Person person : Test.infectionList) {
+			if (name.equals(person.getName())) {
+				System.out.println("이름 : " + person.getName());
 				System.out.println("확진여부 : 확진");
-				System.out.println("나이 : " + person1.getAge());
-				System.out.println("번호  : " + person1.getPhoneNum());
+				System.out.println("나이 : " + person.getAge());
+				System.out.println("번호  : " + person.getPhoneNum());
 			}
 		}
+
+		// 치료자 리스트에서 조회
+		for (Person person : Cure.treatmentCompletedList) {
+			if (name.equals(person.getName())) {
+				System.out.println("이름 : " + person.getName());
+				System.out.println("확진여부 : 완치");
+				System.out.println("나이 : " + person.getAge());
+				System.out.println("번호  : " + person.getPhoneNum());
+			}
+		}
+
+		// 사망자 리스트에서 조회
+		for (Person person : Cure.deathList) {
+			if (name.equals(person.getName())) {
+				System.out.println("이름 : " + person.getName() + " (사망)");
+				System.out.println("나이 : " + person.getAge());
+				System.out.println("번호  : " + person.getPhoneNum());
+			}
+		}
+
+		// 조회 결과가 없다면 이거 어떻게 해야되냐
+//		for (Person person1 : Test.infectionList) {
+//			if (!(name.equals(person1.getName()))) {
+//				System.out.println("infectionList에 해당 이름 없음");
+//				for (Person person2 : Cure.treatmentCompletedList) {
+//					if (!(name.equals(person2.getName()))) {
+//
+//						for (Person person3 : Cure.deathList) {
+//							if (!(name.equals(person3.getName()))) {
+//								System.out.println("검색 결과가 없습니다.");
+//							
+//							} else {
+//								System.out.println("이름 : " + person3.getName() + " (사망)");
+//								System.out.println("나이 : " + person3.getAge());
+//								System.out.println("번호  : " + person3.getPhoneNum());
+//							}
+//						}
+//
+//					} else {
+//						System.out.println("이름 : " + person2.getName());
+//						System.out.println("확진여부 : 완치");
+//						System.out.println("나이 : " + person2.getAge());
+//						System.out.println("번호  : " + person2.getPhoneNum());
+//					}
+//				}
+//				
+//			} else {
+//				System.out.println("이름 : " + person1.getName());
+//				System.out.println("확진여부 : 확진");
+//				System.out.println("나이 : " + person1.getAge());
+//				System.out.println("번호  : " + person1.getPhoneNum());
+//			}
+//		}
 
 	}
 
